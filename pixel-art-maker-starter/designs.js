b79d2263453c235d29d2224fb5f67e95776ff72a
hokkichan <57009866+hokkichan@users.noreply.github.com>
2020-07-22 16:25:29 +0800

    Organising file locations

--- pixel-art-maker-starter/designs.js
+++ pixel-art-maker-starter/designs.js
@@ -2,16 +2,25 @@ const $colorPicker = document.getElementById("colorPicker");
 const $sizePicker = document.getElementById("sizePicker");
 const $table = document.getElementById("pixelCanvas");
 
-function setGrid() {
-  event.preventDefault();
-  let width = document.getElementById("inputWidth").value;
-  let height = document.getElementById("inputHeight").value;
-  makeGrid(width, height);
-}
 
-function makeGrid(width, height) {
-  var row = $table.insertRow(-1);
-  var cell = row.insertCell(-1);
+
+
+
+
+function setGrid(width, height) {
+
+	let width = document.getElementById("inputWidth").value;
+let height = document.getElementById("inputHeight").value;
+  console.log(width)
+
+  //event.preventDefault();
+  //for (var i=0; i<width; i++) {
+  //  var row = document.getElementById("myRow");
+   // var cell = row.insertCell(0);
+  //}
+  
+  
+  
 }
 
 
